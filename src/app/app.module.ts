7f523444742fdcf2d9d507b6f5cd3b3093c8dd90
Murilo Gois <murilogois59@gmail.com>
2023-04-23 19:02:18 -0300

    feat: Add component table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -16,6 +19,9 @@ import { SideBarComponent } from './shared/side-bar/side-bar.component';
 import { MonthComponent } from './views/month/month.component';
 import { CategoryComponent } from './views/category/category.component';
 import { SourceComponent } from './views/source/source.component';
+import { ExpensesTableComponent } from './shared/expenses-table/expenses-table.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -27,14 +33,20 @@ import { SourceComponent } from './views/source/source.component';
     MonthComponent,
     CategoryComponent,
     SourceComponent,
+    ExpensesTableComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    CommonModule,
+    HttpClientModule,
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/expenses-table/expenses-table.component.ts
@@ -0,0 +1,91 @@
+import { AfterViewInit, Component, ViewChild, Input } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { catchError, map, merge, of, startWith, switchMap } from 'rxjs';
+import { IHttpResponse } from 'src/app/interfaces/httpResponse';
+import { IPagination } from 'src/app/interfaces/pagination';
+import { ExpenseModel } from 'src/app/models/expense';
+import { ExpenseService } from 'src/app/services/expense.service';
+
+@Component({
+  selector: 'app-expenses-table',
+  templateUrl: './expenses-table.component.html',
+  styleUrls: ['./expenses-table.component.scss']
+})
+
+export class ExpensesTableComponent implements AfterViewInit {
+  @Input()
+  pageTitle!: string
+
+
+  displayedColumns: string[] = ['orgaoNome','valorEmpenhado', 'valorLiquidado', 'valorPago'];
+  exampleDatabase!: IHttpResponse;
+  data!: ExpenseModel[];
+
+  month = 1;
+
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private expenseService: ExpenseService) {}
+
+  ngAfterViewInit() {
+
+    merge(this.paginator.page)
+    .pipe(
+      startWith({}),
+      switchMap(() => {
+        this.isLoadingResults = true;
+        const params: IPagination = {
+          page: this.paginator.pageIndex,
+          size: this.paginator.pageSize
+        }
+
+        if (this.pageTitle === "Month") {
+          return this.expenseService!
+            .getExpensesByMonth(
+              this.month,
+              params
+            ).pipe(catchError(() => of(null)));
+        } else if(this.pageTitle === "Category") {
+          return this.expenseService!
+          .getExpensesByMonth(
+            this.month,
+            params
+          ).pipe(catchError(() => of(null)));
+        } else if(this.pageTitle === "Source") {
+          return this.expenseService!
+          .getExpensesByMonth(
+            this.month,
+            params
+          ).pipe(catchError(() => of(null)));
+        } else {
+          return this.expenseService!
+          .getExpensesByMonth(
+            this.month,
+            params
+          ).pipe(catchError(() => of(null)));
+        }
+      }),
+      map(data => {
+        this.isLoadingResults = false;
+        this.isRateLimitReached = data === null;
+
+        if (data === null) {
+          return [];
+        }
+
+        this.resultsLength = data.totalElements;
+        return data.content;
+      }),
+    )
+    .subscribe(data => {
+      (this.data = data)
+    });
+  }
+}
