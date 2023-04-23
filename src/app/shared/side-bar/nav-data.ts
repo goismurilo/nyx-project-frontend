05b9059a61ad61f78d098a79cfa8ea09cfbb4616
Murilo Gois <murilogois59@gmail.com>
2023-04-23 01:31:54 -0300

    feat: Add file for NavBar

---
+++ src/app/shared/side-bar/nav-data.ts
@@ -0,0 +1,22 @@
+export const navbarData = [
+  {
+    routeLink: '',
+    label: 'Dashboard',
+    icon: 'home',
+  },
+  {
+    routeLink: '/month',
+    label: 'Despesas por Mês',
+    icon: 'insert_invitation',
+  },
+  {
+    routeLink: '/category',
+    label: 'Despesas por Categoria',
+    icon: 'format_align_left',
+  },
+  {
+    routeLink: '/source',
+    label: 'Dinheiro por Fonte',
+    icon: 'paid',
+  },
+]
