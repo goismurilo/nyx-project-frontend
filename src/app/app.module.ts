dae7c9d86d1548c970c357ef1114758ff3e97aa5
Murilo Gois <murilogois59@gmail.com>
2023-04-24 20:30:40 -0300

    style: Alter Locate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,28 +12,34 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
 
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { SideBarComponent } from './shared/side-bar/side-bar.component';
 import { MonthComponent } from './views/month/month.component';
 import { CategoryComponent } from './views/category/category.component';
 import { SourceComponent } from './views/source/source.component';
 import { ExpensesTableComponent } from './shared/expenses-table/expenses-table.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CardValuesComponent } from './shared/card-values/card-values.component';
 
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+
+registerLocaleData(ptBr);
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    HeaderComponent,
-    FooterComponent,
     SideBarComponent,
     MonthComponent,
     CategoryComponent,
     SourceComponent,
     ExpensesTableComponent,
+    CardValuesComponent,
   ],
   imports: [
     BrowserModule,
@@ -47,8 +53,13 @@ import { CommonModule } from '@angular/common';
     MatTableModule,
     MatProgressSpinnerModule,
     MatPaginatorModule,
+    MatSelectModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
