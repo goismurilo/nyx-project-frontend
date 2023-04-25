4be9bc6c96b812d324042a50fe17aafde1efe1d7
Murilo Gois <murilogois59@gmail.com>
2023-04-24 23:08:34 -0300

    feat(Source): add component Card

--- src/app/views/source/source.component.ts
+++ src/app/views/source/source.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ITotalAmount } from 'src/app/interfaces/totalAmount';
+import { ExpenseService } from 'src/app/services/expense.service';
 
 @Component({
   selector: 'app-source',
   templateUrl: './source.component.html',
   styleUrls: ['./source.component.scss']
 })
-export class SourceComponent {
-  pageTitle = "Source";
-  searchParam = 3;
+export class SourceComponent implements OnInit {
+  pageTitle = "source";
+  searchParam!: number;
+  infoPage!: ITotalAmount;
+  sources!: ITotalAmount[];
+  sourceSelected!: number;
+
+  constructor(private expenseService: ExpenseService) {}
+
+  ngOnInit(): void {
+    this.expenseService.getAllInfoExpenses(this.pageTitle).subscribe(data => {
+      this.infoPage = data[0];
+      this.sources = data;
+      this.sourceSelected = data[0].code;
+      this.searchParam = data[0].code;
+    })
+  }
+
+  onChangeSource(): void {
+    this.searchParam = this.sourceSelected;
+    this.infoPage = this.sources.filter(source => source.code === this.sourceSelected)[0];
+  }
 }
